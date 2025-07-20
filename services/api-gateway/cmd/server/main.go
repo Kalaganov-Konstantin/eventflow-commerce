648b2518e371a3c20658087924e77556896243f5
Kalaganov-Konstantin <kalaganov.konstant@gmail.com>
2025-07-20 14:22:51 +0000

    feat: Implement basic endpoints for all services

--- services/api-gateway/cmd/server/main.go
+++ services/api-gateway/cmd/server/main.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	fmt.Println("Starting service...")
+	fmt.Println("Starting API Gateway service...")
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("OK"))
 	})
 
+	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- services/payment/cmd/server/main.go
+++ services/payment/cmd/server/main.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	fmt.Println("Starting service...")
+	fmt.Println("Starting Payment service...")
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("OK"))
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8082", nil))
 }
